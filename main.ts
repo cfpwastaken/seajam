7c6dc3a574f0b48bcec75318b6783c23597ea9a2
cfp <44261356+cfpwastaken@users.noreply.github.com>
2021-11-27 17:03:05 +0100

    Add more fish types

--- main.ts
+++ main.ts
@@ -30,58 +30,99 @@ for(let i = 0; i < 3; i++) { // SO YOU HAVE TO USE THIS WEIRD `` SYNTAX? OH MY G
     }
 }
 
+// This only created sharks, now I added other fish too and I am too lazy to rename the function
 function createShark() {
     console.log("Creating shark")
+    let fish = randint(0, 2)
     let shark = sprites.create(assets.image`error`, SpriteKind.Enemy)
     let leftOrRight = randint(0, 1)
-    if(leftOrRight == 0) {
-        animation.runImageAnimation(
-            shark,
-            assets.animation`sharkLeft`,
-            100,
-            true
-        )
-        shark.x = scene.screenWidth()
-    } else if(leftOrRight == 1) {
-        animation.runImageAnimation(
-            shark,
-            assets.animation`sharkRight`,
-            100,
-            true
-        )
-        shark.x = 0
+    if(fish == 0) {
+        if (leftOrRight == 0) {
+            animation.runImageAnimation(
+                shark,
+                assets.animation`sharkLeft`,
+                100,
+                true
+            )
+            shark.x = scene.screenWidth()
+        } else if (leftOrRight == 1) {
+            animation.runImageAnimation(
+                shark,
+                assets.animation`sharkRight`,
+                100,
+                true
+            )
+            shark.x = 0
+        }
+    } else if (fish == 1) {
+        if (leftOrRight == 0) {
+            animation.runImageAnimation(
+                shark,
+                assets.animation`angelFishLeft`,
+                100,
+                true
+            )
+            shark.x = scene.screenWidth()
+        } else if (leftOrRight == 1) {
+            animation.runImageAnimation(
+                shark,
+                assets.animation`angelFishRight`,
+                100,
+                true
+            )
+            shark.x = 0
+        }
+    } else if (fish == 2) {
+        if (leftOrRight == 0) {
+            animation.runImageAnimation(
+                shark,
+                assets.animation`clownFishLeft`,
+                100,
+                true
+            )
+            shark.x = scene.screenWidth()
+        } else if (leftOrRight == 1) {
+            animation.runImageAnimation(
+                shark,
+                assets.animation`clownFishRight`,
+                100,
+                true
+            )
+            shark.x = 0
+        }
     }
     shark.y = randint(0, scene.screenHeight())
 
-    game.onUpdateInterval(10, function() {
-        if(leftOrRight == 0) {
+    game.onUpdateInterval(10, function () {
+        if (leftOrRight == 0) {
             shark.x -= 1
-            if(shark.x < 0) {
+            if (shark.x < 0) {
                 shark.x = scene.screenWidth()
                 shark.y = randint(0, scene.screenHeight())
             }
         } else {
             shark.x += 1
-            if(shark.x > scene.screenWidth()) {
+            if (shark.x > scene.screenWidth()) {
                 shark.x = 0
                 shark.y = randint(0, scene.screenHeight())
             }
         }
     })
 }
+
 createShark()
 
 basic.forever(function() {
     if(trash.length == 0) {
         trashAmount += 2
         if(trashAmount > 100) trashAmount = 100
-        if(trashAmount < 10) createShark()
+        if(trashAmount < 15) createShark()
         for(let i = 0; i < trashAmount; i++) {
             let item = sprites.create(assets.image`smallBurger`, SpriteKind.PreFood)
             item.x = randint(0, scene.screenWidth() - 50)
             item.y = randint(0, scene.screenHeight())
             trash.push(item)
-            basic.pause(100)
+            basic.pause(50)
             item.setKind(SpriteKind.Food)
         }
     }
